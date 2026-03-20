efffaefc9eac9ee3337b10c5fbe755d7a2b6c1ea
michaelsrgg <142556446+michaelsrgg@users.noreply.github.com>
2026-03-20 16:41:33 +0700

    p

--- src/main/kotlin/org/course/helpers/DatabaseHelper.kt
+++ src/main/kotlin/org/course/helpers/DatabaseHelper.kt
@@ -1,5 +1,6 @@
 package org.course.helpers
 
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.*
 import org.course.tables.*
 import org.jetbrains.exposed.sql.Database
@@ -7,11 +8,19 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
-    val host     = environment.config.property("ktor.database.host").getString()
-    val port     = environment.config.property("ktor.database.port").getString()
-    val name     = environment.config.property("ktor.database.name").getString()
-    val user     = environment.config.property("ktor.database.user").getString()
-    val password = environment.config.property("ktor.database.password").getString()
+    val dotenv = dotenv {
+        directory       = "."
+        ignoreIfMissing = true
+        systemProperties = true
+    }
+
+    val host     = dotenv["DB_HOST"]     ?: System.getenv("DB_HOST")     ?: "localhost"
+    val port     = dotenv["DB_PORT"]     ?: System.getenv("DB_PORT")     ?: "5432"
+    val name     = dotenv["DB_NAME"]     ?: System.getenv("DB_NAME")     ?: "course_db"
+    val user     = dotenv["DB_USER"]     ?: System.getenv("DB_USER")     ?: "postgres"
+    val password = dotenv["DB_PASSWORD"] ?: System.getenv("DB_PASSWORD") ?: "postgres"
+
+    log.info("Connecting to DB: jdbc:postgresql://$host:$port/$name as $user")
 
     Database.connect(
         url      = "jdbc:postgresql://$host:$port/$name",
@@ -20,7 +29,6 @@ fun Application.configureDatabases() {
         password = password,
     )
 
-    // Auto-create tables if not exist (urutan penting karena foreign key)
     transaction {
         SchemaUtils.createMissingTablesAndColumns(
             UserTable,
